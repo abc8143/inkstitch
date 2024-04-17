11016456dfc61652755f6ba56b14fffab599b3a2
Heroyk26 <a814360602@gmail.com>
2024-04-18 02:25:35 +0800

    删掉这个git version

--- inkstitch.py
+++ inkstitch.py
@@ -12,16 +12,16 @@ from io import StringIO
 
 from lib.exceptions import InkstitchException, format_uncaught_exception
 
-if getattr(sys, 'frozen', None) is None:
-    # When running in development mode, we want to use the inkex installed by
-    # pip install, not the one bundled with Inkscape which is not new enough.
-    if sys.platform == "darwin":
-        extensions_path = "/Applications/Inkscape.app/Contents/Resources/share/inkscape/extensions"
-    else:
-        extensions_path = "/usr/share/inkscape/extensions"
-
-    sys.path.remove(extensions_path)
-    sys.path.append(extensions_path)
+# if getattr(sys, 'frozen', None) is None:
+#     # When running in development mode, we want to use the inkex installed by
+#     # pip install, not the one bundled with Inkscape which is not new enough.
+#     if sys.platform == "darwin":
+#         extensions_path = "/Applications/Inkscape.app/Contents/Resources/share/inkscape/extensions"
+#     else:
+#         extensions_path = "/usr/share/inkscape/extensions"
+#
+#     sys.path.remove(extensions_path)
+#     sys.path.append(extensions_path)
 
 from inkex import errormsg
 from lxml.etree import XMLSyntaxError
